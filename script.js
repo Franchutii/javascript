c449e26d2e2d53dcc6b228af90fcdaa5f40253fe
Jorge Vega <franciscojavier.expositojurado@campusfp.es>
2024-02-01 11:35:28 +0100

    Mi primer ejercicio JS

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Mi primer ejercicio de JS");
+
+let nombre = "TuNombre";
+let apellidos = "TusApellidos";
+let animal = "TuAnimalPreferido";
+console.log(nombre)
+console.log(apellidos);
+console.log(animal);
+
